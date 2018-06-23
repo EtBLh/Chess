119dcbe6900579c3b13f9c02a61fa9de1ba34929
Bill <tszlungchung13768@gmail.com>
2018-06-23 02:57:35 -0700

    new

---
+++ scripts/service/chesstype/queen.js
@@ -0,0 +1,88 @@
+const dto = require('..../dto.js');
+/**
+ * how to get :
+ * var pawn = pawn.getInstance();
+ */
+
+function queen (color) {
+
+    let gameDto = dto.getInstance();
+
+    let map = gameDto.getMap(); // columns starts at 0
+
+    //this.type = 'queen';
+
+    this.canGo = pos => {
+
+        /**
+         *  Input:
+         *      a array, the position of the chess
+         *      'pos' is a array ,refer to the position
+         *
+         *  Output:
+         *      a array, the positions the chess can go
+         *      each element is a position like (x,y)
+         */
+
+        let arr = arr || [];
+
+        let x = pos[0],
+            y = pos[1]; // (x,y) is the position
+
+        let is_cango = coordinate => (map[coordinate[0]][coordinate[1]] !== null && map[coordinate[0]][coordinate[1]].color !== this.color);
+
+        /* cases of canGo*/
+
+        let i = i || 0;
+
+        //case 1: go by row
+        i = 0;
+        while (y+i<=7 && is_cango([x,y+i])) {
+            arr.push([x,y+i]);
+            i++;
+        }
+        i = 0;
+        while (y-i>=0 && is_cango([x,y-i])) {
+            arr.pusj([x,y-i]);
+            i++;
+        }
+
+        //case 2: go by columns
+        i = 0;
+        while (x+i<=7 && is_cango([x+i,y])) {
+            arr.push([x+i,y]);
+            i++;
+        }
+        i = 0;
+        while (x-i>=0 && is_cango([x+i,y])) {
+            arr.push([x-i,y]);
+            i++;
+        }
+
+        //case 3: right tilted
+        i = 0;
+        while (x+i<=8 && y+i<=8 && is_cango([x+i][y+i])) {
+            arr.push([x+i,y+i]);
+            i++;
+        }
+        i = 0;
+        while (x-i>=0 && y-i>=0 && is_cango([x-i,y-i])) {
+            arr.push([x-i,y-i]));
+            i++;
+        }
+
+        //case 4: left tilted
+        i = 0;
+        while (x-i<=8 && y+i<=8 && is_cango([x-i][y+i])) {
+            arr.push([x+i,y+i]);
+            i++;
+        }
+        i = 0;
+        while (x+i>=0 && y-i>=0 && is_cango([x+i,y-i])) {
+            arr.push([x-i,y-i]));
+            i++;
+        }
+
+        return arr;
+    };
+}
