8abf73963561776111d1a611f845412b188927a5
etb <Hugo432121@gmail.com>
2018-11-15 14:50:21 +0800

    project restart

--- scripts/service/playerStream.js
+++ scripts/service/playerStream.js
@@ -1,14 +1,24 @@
 import dto from '../dto.js'
 
 /**
+ * 
+ * playerStream @description manage human input and AI input
+ * 
+ * @class @public
+ * @requires dto
+ * 
  * @param {Object} data - record the user assignment( human or AI? )
  * @param {String} data.black - "human" or "AI"
  * @param {String} data.white - "human" or "AI"
  */
-export default playerStream = ( data ) => {
+
+export default playerStream = 
+( data = {black: "human", white: "human"}, mouseClicked) => {
         
+    //get instance of dto
     const gameDto = dto.getInstance();
 
+    
     const human_play = () =>{
 
     }
